627d894c9b818787c9d76dd96acf1a68b8ecc3f7
zemgh <bast3ran@yandex.ru>
2024-12-29 20:02:50 +0300

    third try

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import multiprocessing
+import subprocess
+
 from fastapi import FastAPI
 from prometheus_client import Counter
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -13,6 +16,15 @@ instrumentator.expose(app, endpoint="/metrics")
 @app.get('/')
 async def root():
     hello_counter.inc(amount=1)
-    return {'message': 'Hello World'}
+    return {'service_status': "I'm fine"}
 
+
+def start_worker():
+    subprocess.run(['python', 'worker.py'])
+
+
+@app.on_event("startup")
+async def startup_event():
+    worker_process = multiprocessing.Process(target=start_worker)
+    worker_process.start()
 
